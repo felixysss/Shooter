998f3714b4dcffb138cb9c4a78e0ed25ae1a0971
felixys <maelaquardon1@gmail.com>
2023-11-25 17:11:39 +0100

    maj

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -13,6 +13,6 @@ public class ScoreScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        textQuiAfficheLeScore.text = " Score "  +score;
+        textQuiAfficheLeScore.text = " Ton score est de : "  +score;
     }
 }
